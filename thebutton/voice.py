4df7a301ddb5af0e68f332c2ed632ca195e0fbdc
Andrew Burrows <burrowsa@gmail.com>
2018-07-03 00:20:21 +0100

    Don't say empty messages

--- thebutton/voice.py
+++ thebutton/voice.py
@@ -4,4 +4,5 @@ PREFIX = ".." # Work around the startup time for HDMI audio
 
 class Voice:
     def say(self, message):
-        os.system("echo '{}{}' | festival --tts &".format(PREFIX, message))
+        if message:
+            os.system("echo '{}{}' | festival --tts &".format(PREFIX, message))
