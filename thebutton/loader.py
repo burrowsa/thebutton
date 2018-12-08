32c7dc4a002f10d40fbf3f320d6d16f1bed13d26
Andrew Burrows <burrowsa@gmail.com>
2018-12-08 22:17:13 +0000

    Keep track of which challenges have been completed already

--- thebutton/loader.py
+++ thebutton/loader.py
@@ -1,10 +1,28 @@
-import os, random
+import os, random, pickle
 from thebutton.parser import parse
 from thebutton.standardwaitstep import StandardWaitStep
 
 
+COMPLETED_CHALLENGES_PATH = "completed_challenges.pkl"
+
+
+def load_completed():
+    try:
+        with open(COMPLETED_CHALLENGES_PATH, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return set()
+
+
+def save_completed(completed):
+    with open(COMPLETED_CHALLENGES_PATH, "wb") as f:
+        return pickle.dump(completed, f)
+
+
 def load_all_challenges(path, randomise):
-    all_files = os.listdir(path)
+    completed = load_completed()
+    all_files = list(set(os.listdir(path)) - completed)
+
     if randomise:
         random.shuffle(all_files)
     else:
@@ -14,4 +32,5 @@ def load_all_challenges(path, randomise):
         if filename.endswith(".json"):
             yield StandardWaitStep
             yield from parse(os.path.join(path, filename))
-
+            completed.add(filename)
+            save_completed(completed)
