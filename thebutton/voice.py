50994b35bfb39e1f8c2cd9d16194fa63d602e35a
Andrew Burrows <burrowsa@gmail.com>
2018-12-08 22:09:33 +0000

    Speech doesn't work, scrap it

--- thebutton/voice.py
+++ thebutton/voice.py
@@ -1,8 +1,4 @@
-import os
-
-PREFIX = "" # Add some full stops to Work around the startup time for HDMI audio if required
-
 class Voice:
     def say(self, message):
-        if message:
-            os.system("echo '{}{}' | festival --tts &".format(PREFIX, message))
+        # TODO: add working speech, previous attempts have been laggy and rubbish
+        pass
