28a86fe245f6e8d4a2eecd205f2a76f8bd7bd328
Andrew Burrows <burrowsa@gmail.com>
2018-12-08 22:10:15 +0000

    Debounce button presses by 3s

--- thebutton/button.py
+++ thebutton/button.py
@@ -1,15 +1,20 @@
 import RPi.GPIO as GPIO
 from threading import Event
 from time import sleep
+from datetime import timedelta, datetime as dt
+
 
 BLUE = '#0e1a64'
 GREEN = 2
 RED = 3
 BUTTON = 4
+THREE_SECONDS = timedelta(seconds=3)
+
 
 class Button:
     def __init__(self):
         self.event = Event()
+        self.lastPressTime = dt.now()
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(GREEN, GPIO.OUT)
         GPIO.setup(RED, GPIO.OUT)
@@ -19,9 +24,10 @@ class Button:
         self.green()
 
     def on_button_press(self, pin):
-        if GPIO.input(pin) == False:
+        if GPIO.input(pin) == False and dt.now() - self.lastPressTime > THREE_SECONDS:
             self.event.set()
-            sleep(1)
+            self.lastPressTime = dt.now()
+            
     
     def red(self):
         GPIO.output(GREEN, False)
